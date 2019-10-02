28fd8fe338f68779078656249e1b5d1ad1c8f5a8
Zuzeth Romo <zuzethromo@yahoo.com>
2019-10-02 13:42:05 -0600

    file paths and most of files set up

---
+++ server.js
@@ -0,0 +1,24 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let methodOverride = require('method-override');
+let exphbs = require('express-handlebars');
+
+let app = express();
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.use(methodOverride('_method'));
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
+let routes = require('./controllers/routes.js');
+app.use('/',routes);
+
+let port = 3000;
+app.listen(port);
